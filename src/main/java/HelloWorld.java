644e3ae08466b6fcec359f7ed21b7016f0a85410
schochk <k.schoch>
2015-05-06 13:12:48 +0200

    a

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -6,7 +6,8 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("hello world!");
-        System.out.println("Hello Mirjam!");}
+        System.out.println("Hello Mirjam!");
+        System.out.println("Hello Mirjam2!");}
 
 
 
