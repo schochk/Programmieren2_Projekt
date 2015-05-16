3dbcd29fe96ddb24284f3d484ba19480fdb283c6
schochk <k.schoch>
2015-05-16 11:23:05 +0200

    UserInterface

--- src/main/java/gui/ScatterPlot.java
+++ src/main/java/gui/ScatterPlot.java
@@ -11,6 +11,11 @@ import java.awt.geom.Ellipse2D;
  */
 public class ScatterPlot extends JPanel {
 
+    double vxListe[] = {262, 319, 364, 400, 418};
+    double vyListe[] = {400, 388, 363, 320, 262};
+
+
+
     int q=0;
 
     public void scatterPlotComponent() {
@@ -30,39 +35,42 @@ public class ScatterPlot extends JPanel {
         });
 
 
+
     }
 
     public void paintComponent(Graphics g) {
+
 
         Graphics2D g2d = (Graphics2D) g;
         g2d.setColor(Color.ORANGE);
         g2d.fillRect(0, 0, this.getWidth(), this.getHeight());
         g2d.setColor(Color.BLACK);
         int mx = ((getWidth()/100)*50);
-        int my = ((getHeight()/100)*25);
-        int ax = (getWidth()/100)*25;
-        int ay = (getHeight()/100)*75;
-        int bx = ((getWidth()/100)*75);
+        int my = ((getHeight()/100)*50);
+        int ax = (getWidth()/418)*418;
+        int ay = (getHeight()/500)*200;
+        /*int bx = ((getWidth()/100)*75);
         int by = ((getHeight()/100)*75);
-
+*/
         int r = 5;
         Ellipse2D.Double m = new Ellipse2D.Double(mx - r, my - r, 2 * r, 2 * r);
         Ellipse2D.Double a = new Ellipse2D.Double(ax - r, ay - r, 2 * r, 2 * r);
-        Ellipse2D.Double b = new Ellipse2D.Double(bx - r, by - r, 2 * r, 2 * r);
+        //Ellipse2D.Double b = new Ellipse2D.Double(bx - r, by - r, 2 * r, 2 * r);
 
         g2d.fill(m);
         g2d.fill(a);
-        g2d.fill(b);
+        //g2d.fill(b);
 
-        if(q==1) {
+        /*if(q==1) {
             g.drawLine(mx, my, ax, ay);
-            g.drawLine(ax, ay, bx, by);
-            g.drawLine(bx, by, mx, my);
-        }
+          //  g.drawLine(ax, ay, bx, by);
+            //g.drawLine(bx, by, mx, my);
+        }*/
 
         updateUI();
 
     }
+
 
 }
 
