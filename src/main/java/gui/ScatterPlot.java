ef3c3fb92a87bb2f3ee35a4518a141267cfdf0eb
schochk <k.schoch>
2015-05-20 15:10:47 +0200

    StreuPlot

--- src/main/java/gui/ScatterPlot.java
+++ src/main/java/gui/ScatterPlot.java
@@ -14,11 +14,11 @@ import java.util.List;
 public class ScatterPlot extends JPanel {
 
 
-    List<Integer> xList = Arrays.asList(262, 319, 364, 400, 418);
+    List<Integer> xList = Arrays.asList(2620,3190,3640,4000,4180,4150,3860,3520,3080,2630,2050,1610,1300,1090,1130,1340,1680,2140,1980,2000,2250,2220,2960,2930,3170,3140,3500,3280,2890,2500,2180,1860);
     double maxvx = Collections.max(xList);
     double minvx = Collections.min(xList);
 
-    List<Integer> yList = Arrays.asList(400, 388, 363, 320, 262);
+    List<Integer> yList = Arrays.asList(4000,3880,3620,3200,2620,2030,1470,1170,960,880,1010,1280,1680,2200,2730,3260,3660,3910,2980,2780,2760,3010,3050,2860,3070,2860,2120,1780,1630,1610,1710,2020);
     double maxvy = Collections.max(yList);
     double minvy = Collections.min(yList);
 
@@ -62,16 +62,13 @@ public class ScatterPlot extends JPanel {
         int r = 5;
 
         g.fillOval(mx-r, my-r, 2*r, 2*r);
-        //g.fillOval(ax-r, ay-r, 2*r, 2*r);
+
 
         for(int i = 0; i < xList.size(); i++){
-            //double x = xList.get(i);
-            //double y = yList.get(i);
 
             g.fillOval((int)(xd * (xList.get(i)-minvx)-r), (int)(getHeight()-(yd*(yList.get(i)-minvy))-r),2*r,2*r);
 
         }
-
 
         /*if(q==1) {
             g.drawLine(mx, my, ax, ay);
